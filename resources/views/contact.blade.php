e3c36f8fb06e5dadce74581b10eee41403415d80
Marvin jay G. Rubio <marvinjay.rubio@ssu.edu.ph>
2023-08-18 16:58:04 +0800

    add bootstrap locally

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,10 +7,10 @@
         <title>Laravel</title>
 
       <!-- Bootstrap CSS -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-        
+      <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
       <!-- Bootstrap JS -->
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+      <script src="/bootstrap/js/bootstrap.min.js"></script>
+      
     </head>
     <body class="container">
         <div class="container">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
         <title>Laravel</title>
 
       <!-- Bootstrap CSS -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-        
+      <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
       <!-- Bootstrap JS -->
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-    </head>
+      <script src="/bootstrap/js/bootstrap.min.js"></script>
+      
+      </head>
     <body class="container">
         <div class="container">
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
